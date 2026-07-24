f39fd65b4cf6574b006e6f4a04863a228452dd40
Hector Zelaya <hector@hectorzelaya.dev>
2026-07-24 15:53:05 -0600

    simplified retroarch

--- features/apps/gaming/user.nix
+++ features/apps/gaming/user.nix
@@ -6,6 +6,15 @@
     #protonup-qt
     #mangohud
     eden
-    retroarch-full
+    (retroarch.withCores (cores: with cores; [
+      melondsds       # Nintendo DS
+      gambatte        # Game Boy / Game Boy Color
+      mgba            # Game Boy Advance
+      dolphin         # GameCube / Wii
+      mupen64plus     # Nintendo 64 (Mupen64Plus-Next)
+      snes9x          # SNES
+      genesis-plus-gx # Mega Drive / Genesis
+      ppsspp          # PSP
+    ]))
   ];
 }
